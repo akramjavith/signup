198df12d7ea48169afe42a584af5a4072148fca7
javith <akramjavithkhan@gmail.com>
2022-06-30 10:26:32 +0530

    updated js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const password = document.querySelector('#password');
-const confirm = document.querySelector('#confirm');
-const alert = document.querySelector('.password-alert');
+const alert = document.querySelector(".password-alert");
+const password = document.querySelector("#password");
+const confirm = document.querySelector("#confirm");
 
+password.addEventListener("keyup", e => {
+    if(password.value !== confirm.value){
+        alert.style.display = "inline-block";
+    }
+})
+
+confirm.addEventListener("keyup", e => {
+    if(password.value == confirm.value){
+        alert.style.display = "none";
+        document.querySelector('input[type="password"]:focus');
+        alret.style.outlineColor = "green";
+    }
+})
